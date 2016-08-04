5502b1912cae7a70632193ca54da90f0efa7d918
brandon <brandon4261@gmail.com>
2016-08-04 21:19:46 +1000

    Added GuiUtils

---
+++ src/main/java/teamasm/moh/client/gui/GuiUtils.java
@@ -0,0 +1,8 @@
+package teamasm.moh.client.gui;
+
+/**
+ * Created by brandon3055 on 4/08/2016.
+ * This class will contain a bunch of useful methods for rendering and gui elements as well as other gui related things.
+ */
+public class GuiUtils {
+}
