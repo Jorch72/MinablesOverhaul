b4221d1cbf618adc4770a1990a78175256ea12e6
brandon <brandon4261@gmail.com>
2016-08-04 23:23:03 +1000

    Added ore block

--- src/main/java/teamasm/moh/block/BlockOre.java
+++ src/main/java/teamasm/moh/block/BlockOre.java
@@ -1,12 +1,22 @@
 package teamasm.moh.block;
 
+import codechicken.lib.block.BlockMultiTile;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 /**
  * Created by brandon3055 on 4/08/2016.
  */
-public class BlockOre extends BaseBlock {
+public class BlockOre extends BlockMultiTile implements ITileEntityProvider{
+
     public BlockOre() {
         super(Material.ROCK);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return null;
     }
 }
--- src/main/java/teamasm/moh/init/ModBlocks.java
+++ src/main/java/teamasm/moh/init/ModBlocks.java
@@ -1,18 +1,21 @@
 package teamasm.moh.init;
 
-import net.minecraft.block.Block;
+import codechicken.lib.block.ItemBlockMultiType;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import teamasm.moh.block.BlockOre;
+import teamasm.moh.block.tileentity.TileOre;
 
 /**
  * Created by brandon3055 on 4/08/2016.
  */
 public class ModBlocks {
 
-    public static Block blockOre = new BlockOre().setRegistryName("blockOre");
+    public static BlockOre blockOre = new BlockOre();
 
     public static void init() {
-        GameRegistry.register(blockOre);
+        GameRegistry.register(blockOre.setRegistryName("blockOre"));
+        GameRegistry.register(new ItemBlockMultiType(blockOre).setRegistryName("blockOre"));
+        blockOre.registerSubItemAndTile(0, "blockOre", TileOre.class);
     }
 
 }
