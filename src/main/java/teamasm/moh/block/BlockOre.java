5af3d73b013a79bb222190375d7c0c055cdf0209
brandon <brandon4261@gmail.com>
2016-08-04 23:56:45 +1000

    Debugging

--- src/main/java/teamasm/moh/block/BlockOre.java
+++ src/main/java/teamasm/moh/block/BlockOre.java
@@ -3,8 +3,11 @@ package teamasm.moh.block;
 import codechicken.lib.block.BlockMultiTile;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import teamasm.moh.block.tileentity.TileOre;
 
 /**
  * Created by brandon3055 on 4/08/2016.
@@ -16,7 +19,18 @@ public class BlockOre extends BlockMultiTile implements ITileEntityProvider{
     }
 
     @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        TileEntity tile = worldIn.getTileEntity(pos);
+
+        if (tile instanceof TileOre) {
+            tile.markDirty();
+        }
+
+        super.onBlockAdded(worldIn, pos, state);
+    }
+
+    @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return null;
+        return new TileOre();
     }
 }
