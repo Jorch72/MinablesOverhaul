fa7e4a3660c45a1f616d13ede52126db88389ec0
brandon <brandon4261@gmail.com>
2016-08-05 20:53:04 +1000

    updatedVarients

--- src/main/java/teamasm/moh/reference/VariantReference.java
+++ src/main/java/teamasm/moh/reference/VariantReference.java
@@ -1,13 +1,9 @@
 package teamasm.moh.reference;
 
-import codechicken.lib.colour.Colour;
-import codechicken.lib.colour.ColourRGBA;
 import codechicken.lib.util.ArrayUtils;
 import com.google.common.collect.Lists;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by covers1624 on 8/5/2016.
@@ -20,6 +16,9 @@ public class VariantReference {
     public static final String[] miscOres = new String[] { "oreLapis", "oreDiamond", "oreRedstone", "oreEmerald", "oreQuartz", "oreCoal", "oreRuby", "oreSapphire", "orePeridot" };
     public static final List<String> miscOresList = Lists.newArrayList(ArrayUtils.arrayToLowercase(miscOres));
 
-    public static final String[] machines = new String[] {};
+    public static final String[] machines = new String[] {"reducerCrusher", "reducerGrinder", "screenCoarse", "screenMedium", "screenFine", "separatorMagnetic", "separatorGravity", "separatorFlotation", "separatorElectrostatic", "dryerRotary"};
     public static final List<String> machinesList = Lists.newArrayList(ArrayUtils.arrayToLowercase(machines));
+
+    public static final String[] manualMachines = new String[] {"reducerManual", "screenManual", "separatorManual", "dryerManual"};
+    public static final List<String> manualMachinesList = Lists.newArrayList(ArrayUtils.arrayToLowercase(manualMachines));
 }
