c03ac9cbb91ffd7c8f819a184f4d2e9f1f41f9e3
gigabit101 <thegigabit101@gmail.com>
2016-08-05 23:07:12 +0100

    add energy bar last or tooltip overlaps others

--- src/main/java/teamasm/moh/client/gui/GuiReducerCrusher.java
+++ src/main/java/teamasm/moh/client/gui/GuiReducerCrusher.java
@@ -2,6 +2,7 @@ package teamasm.moh.client.gui;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.util.EnumFacing;
 import teamasm.moh.container.ContainerReducerCrusher;
 import teamasm.moh.tile.machines.teir1.TileReducerCrusher;
 
@@ -19,7 +20,12 @@ public class GuiReducerCrusher extends GuiContainer {
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         GuiUtils.drawDefaultBackground(this, guiLeft, guiTop, xSize, ySize);
-        GuiUtils.drawEnergyBar(this, guiLeft + 5, guiTop + 5, 70, tileReducerCrusher.getWorld().rand.nextInt(512000), 512000, mouseX, mouseY);
+        GuiUtils.drawString(this, "Reducer Crusher", guiLeft + 45, guiTop + 10);
+        GuiUtils.drawSlot(this, guiLeft + 40, guiTop + 30);
+        GuiUtils.drawSlot(this, guiLeft + 120, guiTop + 30);
+
+
+        GuiUtils.drawEnergyBar(this, guiLeft + 5, guiTop + 5, 70, tileReducerCrusher.getEnergyStored(EnumFacing.DOWN), tileReducerCrusher.getMaxEnergyStored(EnumFacing.DOWN), mouseX, mouseY);
         GuiUtils.drawPlayerSlots(this, guiLeft + xSize / 2, guiTop + 80, true);
     }
 }
