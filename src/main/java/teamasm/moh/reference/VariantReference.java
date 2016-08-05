0228c6f5cd4128ee17f8312128260be5a988a82e
brandon <brandon4261@gmail.com>
2016-08-05 16:17:14 +1000

    Started work on texture stuff

--- src/main/java/teamasm/moh/reference/VariantReference.java
+++ src/main/java/teamasm/moh/reference/VariantReference.java
@@ -3,7 +3,9 @@ package teamasm.moh.reference;
 import codechicken.lib.util.ArrayUtils;
 import com.google.common.collect.Lists;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by covers1624 on 8/5/2016.
@@ -15,4 +17,10 @@ public class VariantReference {
 
     public static final String[] miscOres = new String[] { "oreLapis", "oreDiamond", "oreRedstone", "oreEmerald", "oreQuartz", "oreCoal", "oreRuby", "oreSapphire", "orePeridot" };
     public static final List<String> miscOresList = Lists.newArrayList(ArrayUtils.arrayToLowercase(miscOres));
+
+    public static final Map<String, Integer> colourMap = new HashMap<String, Integer>();
+
+    static {
+        colourMap.put("oreGold", 0xFF0000);//Example will need to work out the colours for everything later
+    }
 }
