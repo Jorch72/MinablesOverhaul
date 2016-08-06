8761d96fc3afcacec76be5124e2241f2cc27c637
gigabit101 <thegigabit101@gmail.com>
2016-08-06 01:02:54 +0100

    added the rest of the machine blocks / tiles to the game

--- src/main/java/teamasm/moh/init/ModBlocks.java
+++ src/main/java/teamasm/moh/init/ModBlocks.java
@@ -5,8 +5,10 @@ import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import teamasm.moh.block.BlockOre;
 import teamasm.moh.block.BlockMachine;
-import teamasm.moh.tile.machines.teir1.TileReducerCrusher;
+import teamasm.moh.tile.machines.teir1.*;
 import teamasm.moh.reference.Reference;
+import teamasm.moh.tile.machines.tier2.TileReducerGrinder;
+import teamasm.moh.tile.machines.tier2.TileSeperatorElectrostatic;
 
 /**
  * Created by brandon3055 on 4/08/2016.
@@ -28,6 +30,15 @@ public class ModBlocks {
         GameRegistry.register(new ItemBlockMultiType(blockMachine).setRegistryName("machine"));
 
         blockMachine.registerSubItemAndTile(0, "reducerCrusher", TileReducerCrusher.class);
+        blockMachine.registerSubItemAndTile(1, "reducerGrinder", TileReducerGrinder.class);
+        blockMachine.registerSubItemAndTile(2, "screenCoarse", TileScreenCoarse.class);
+        blockMachine.registerSubItemAndTile(3, "screenMedium", TileScreenMedium.class);
+        blockMachine.registerSubItemAndTile(4, "screenFine", TileScreenFine.class);
+        blockMachine.registerSubItemAndTile(5, "separatorMagnetic", TileSeperatorMagnetic.class);
+        blockMachine.registerSubItemAndTile(6, "separatorGravity", TileSeperatorGravity.class);
+        blockMachine.registerSubItemAndTile(7, "separatorFlotation", TileSeperatorFlotation.class);
+        blockMachine.registerSubItemAndTile(8, "separatorElectrostatic", TileSeperatorElectrostatic.class);
+        blockMachine.registerSubItemAndTile(9, "dryerRotary", TileDryerRotary.class);
 
     }
 
