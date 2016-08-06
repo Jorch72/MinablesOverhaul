cddc14fc72823773a96c9bbfe6ff48c1deebb00f
gigabit101 <thegigabit101@gmail.com>
2016-08-06 13:23:55 +0100

    rework machine

--- src/main/java/teamasm/moh/tile/machines/teir1/TileReducerCrusher.java
+++ src/main/java/teamasm/moh/tile/machines/teir1/TileReducerCrusher.java
@@ -12,6 +12,8 @@ import teamasm.moh.item.ItemOre;
 import teamasm.moh.reference.GuiIds;
 import teamasm.moh.tile.TileProcessEnergy;
 
+import java.util.Map;
+
 /**
  * Created by brandon3055 on 5/08/2016.
  */
@@ -22,27 +24,51 @@ public class TileReducerCrusher extends TileProcessEnergy implements IGuiTile, I
     }
 
     int runCost = 100;
+    int SLOT_INPUT = 0;
+    int SLOT_OUTPUT = 1;
 
     @Override
     public void update() {
         if (!worldObj.isRemote) {
-            if (getEnergyStored(EnumFacing.DOWN) >= runCost && getStackInSlot(0) != null && getStackInSlot(0).getItem() instanceof ItemOre) {
-                int stacksize = getStackInSlot(0).stackSize;
-                ItemOre itemOre = (ItemOre) getStackInSlot(0).getItem();
-                if (stacksize > 1 && itemOre.getParticleSize(getStackInSlot(0)) > 0.1) {
-                    //set new Particle size and copy
-                    //                    itemOre.setParticleSize(getStackInSlot(0), itemOre.getParticleSize(getStackInSlot(0)) / stacksize);
-                    itemOre.setReduced(getStackInSlot(0), true);
-                    ItemStack copyStack = getStackInSlot(0);
-                    copyStack.stackSize = 1;
-                    //use power
-                    energyStorage.extractEnergy(runCost, false);
-                    //Remove input stack
-                    setInventorySlotContents(0, null);
-                    //set output stack
-                    setInventorySlotContents(1, copyStack);
+            if(canWork()){
+                progress++;
+                if(progress == 100) {
+                    work();
                 }
+
             }
+        }
+    }
+
+    public boolean canWork(){
+        if(getEnergyStored(EnumFacing.DOWN) >= runCost && getStackInSlot(SLOT_INPUT) != null && getStackInSlot(SLOT_INPUT).getItem() instanceof ItemOre){
+            return true;
+        }
+        return false;
+    }
+
+    public void work() {
+        ItemStack wipStack = getStackInSlot(SLOT_INPUT).copy();
+        wipStack.stackSize = 1;
+        if(wipStack == null) {
+            decrStackSize(SLOT_INPUT, 1);
+        }
+        ItemOre itemOre = (ItemOre) wipStack.getItem();
+        itemOre.setReduced(wipStack, true);
+
+        Map<String, Float> ores = itemOre.getOres(getStackInSlot(SLOT_INPUT));
+        for (String name : ores.keySet()){
+            float newValue = itemOre.getOres(getStackInSlot(SLOT_INPUT)).get(name).floatValue() + itemOre.getOres(wipStack).get(name).floatValue();
+            itemOre.modifyPurity(name, newValue, wipStack);
+        }
+        decrStackSize(SLOT_INPUT, 1);
+        progress = 0;
+
+        //output
+        if(getStackInSlot(SLOT_OUTPUT) == null && getStackInSlot(SLOT_INPUT) == null) {
+            setInventorySlotContents(SLOT_OUTPUT, wipStack);
+            wipStack = null;
+            progress = 0;
         }
     }
 
