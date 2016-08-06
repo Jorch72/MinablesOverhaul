73c4b6db80d71dcd453b434009d59d18930d17c3
brandon <brandon4261@gmail.com>
2016-08-06 10:57:34 +1000

    Adjusted the base purity

--- src/main/java/teamasm/moh/init/Ores.java
+++ src/main/java/teamasm/moh/init/Ores.java
@@ -8,24 +8,24 @@ import teamasm.moh.reference.OreRegistry;
 public class Ores {
 
     public static void init() {
-        OreRegistry.INSTANCE.registerOre("oreGold", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreIron", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreAluminum", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreCopper", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreLead", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreTin", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreSilver", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreUranium", 1, 1, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreGold", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreIron", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreAluminum", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreCopper", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreLead", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreTin", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreSilver", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreUranium", 1, 0.05F, 1);//TODO
 
-        OreRegistry.INSTANCE.registerOre("oreLapis", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreDiamond", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreRedstone", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreEmerald", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreQuartz", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreCoal", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreRuby", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("oreSapphire", 1, 1, 1);//TODO
-        OreRegistry.INSTANCE.registerOre("orePeridot", 1, 1, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreLapis", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreDiamond", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreRedstone", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreEmerald", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreQuartz", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreCoal", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreRuby", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("oreSapphire", 1, 0.05F, 1);//TODO
+        OreRegistry.INSTANCE.registerOre("orePeridot", 1, 0.05F, 1);//TODO
     }
 
 }
