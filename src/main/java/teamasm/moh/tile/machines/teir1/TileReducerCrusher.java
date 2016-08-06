804d2f97f43b0e5e72c2734664581a279d9dfdea
gigabit101 <thegigabit101@gmail.com>
2016-08-06 02:09:48 +0100

    start on Crusher 2am whoops!!

--- src/main/java/teamasm/moh/tile/machines/teir1/TileReducerCrusher.java
+++ src/main/java/teamasm/moh/tile/machines/teir1/TileReducerCrusher.java
@@ -2,10 +2,13 @@ package teamasm.moh.tile.machines.teir1;
 
 import codechicken.lib.tile.IGuiTile;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import teamasm.moh.api.recipe.IMOHRecipe;
+import teamasm.moh.item.ItemOre;
 import teamasm.moh.reference.GuiIds;
 import teamasm.moh.tile.TileProcessEnergy;
 
@@ -17,10 +20,29 @@ public class TileReducerCrusher extends TileProcessEnergy implements IGuiTile, I
     public TileReducerCrusher() {
         setInventory(2, 64);
     }
+    int runCost = 100;
 
     @Override
     public void update() {
-
+        if(!worldObj.isRemote) {
+            if (getEnergyStored(EnumFacing.DOWN) >= runCost && getStackInSlot(0) != null && getStackInSlot(0).getItem() instanceof ItemOre) {
+                int stacksize = getStackInSlot(0).stackSize;
+                ItemOre itemOre = (ItemOre) getStackInSlot(0).getItem();
+                if (stacksize > 1 && itemOre.getParticleSize(getStackInSlot(0)) > 0.1) {
+                    //set new Particle size and copy
+//                    itemOre.setParticleSize(getStackInSlot(0), itemOre.getParticleSize(getStackInSlot(0)) / stacksize);
+                    itemOre.setReduced(getStackInSlot(0), true);
+                    ItemStack copyStack = getStackInSlot(0);
+                    copyStack.stackSize = 1;
+                    //use power
+                    energyStorage.extractEnergy(runCost, false);
+                    //Remove input stack
+                    setInventorySlotContents(0, null);
+                    //set output stack
+                    setInventorySlotContents(1, copyStack);
+                }
+            }
+        }
     }
 
     @Override
